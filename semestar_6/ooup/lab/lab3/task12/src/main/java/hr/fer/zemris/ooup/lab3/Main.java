7dbad09beb882618c1ce83f0985e7a000171fafd
Tomislav Bjelčić <tomislav.bjelcic@fer.hr>
2021-05-13 03:01:29 +0200

    gotov 1. zadatak

--- semestar_6/ooup/lab/lab3/task12/src/main/java/hr/fer/zemris/ooup/lab3/Main.java
+++ semestar_6/ooup/lab/lab3/task12/src/main/java/hr/fer/zemris/ooup/lab3/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		String[] kinds = new String[] {"Dog", "Parrot", "Cat"};
+		String[] kinds = new String[] {"Dog", "Parrot", "Cat", "Tiger", "Dog", "Parrot", "Tiger"};
 		String name = "Mirko";
 		
 		for (String kind : kinds) {
