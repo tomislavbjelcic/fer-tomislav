31e157dcfc6f29f7267f073eda59db43855d3e90
Tomislav Bjelčić <tomislav.bjelcic@fer.hr>
2021-08-28 09:47:43 +0200

    fzawg78fgat6f

--- semestar_6/zavrad/programs/elbow.py
+++ semestar_6/zavrad/programs/elbow.py
@@ -1,10 +1,7 @@
 from sklearn.datasets import make_blobs
 from sklearn.cluster import KMeans
-from sklearn.metrics import silhouette_samples, silhouette_score
 
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-import numpy as np
 
 def plot_points(x, y, dotsize):
     plt.figure(figsize=(6, 6))
